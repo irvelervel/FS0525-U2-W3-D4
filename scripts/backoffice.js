236cab037206c7d4decb7fee2cb2431eedf69dba
Stefano Casasola <ste.casasola@gmail.com>
2025-11-06 13:12:32 +0100

    Finishes TicketONE!

--- scripts/backoffice.js
+++ scripts/backoffice.js
@@ -5,6 +5,42 @@ const getYearInFooter = function () {
 
 getYearInFooter()
 
+const eventsURL = 'https://striveschool-api.herokuapp.com/api/agenda'
+// è lo stesso endpoint che usavamo per la GET!
+
+// -- PARTE FINALE DELLA LEZIONE --
+// cerco nell'URL se esiste un parametro chiamato "concertID"
+// se esiste, vuol dire che questa pagina backoffice è stata caricata in modalità "modifica"
+// se NON esiste, vuol dire che questa pagina backoffce è stata caricata in semplice modalità "creazione"
+const url = location.search
+const allTheParameters = new URLSearchParams(url)
+// provo a estrapolare "concertID"
+const id = allTheParameters.get('concertID')
+// id se sono in MODIFICA -> esiste
+// id se sono in CREAZIONE -> non esiste
+
+if (id) {
+  // vuol dire che ID non è null
+  fetch(eventsURL + '/' + id)
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw new Error(res.status)
+      }
+    })
+    .then((eventDetails) => {
+      // ho ottenuto i dettagli, ripopolo ora il form
+      document.getElementById('name').value = eventDetails.name // il nome dell'evento
+      document.getElementById('description').value = eventDetails.description // la desc. dell'evento
+      document.getElementById('price').value = eventDetails.price // il prezzo dell'evento
+      document.getElementById('time').value = eventDetails.time.slice(0, -5) // la data dell'evento
+    })
+    .catch((err) => {
+      console.log('errore nel ripopolamento del form', err)
+    })
+}
+
 class Concert {
   constructor(_name, _description, _price, _time) {
     this.name = _name
@@ -47,10 +83,30 @@ form.addEventListener('submit', (e) => {
   console.log('NEWEVENT', newEvent)
 
   // il concerto è pronto! ora lo INVIAMO alle API in modo da SALVARLO nel DB
-  const eventsURL = 'https://striveschool-api.herokuapp.com/api/agenda'
-  // è lo stesso endpoint che usavamo per la GET!
-  fetch(eventsURL, {
-    method: 'POST', // creo una nuova risorsa
+
+  // se l'id è presente nell'URL devo fare una PUT, non devo fare una POST!
+
+  let method
+
+  if (id) {
+    method = 'PUT'
+  } else {
+    method = 'POST'
+  }
+
+  let finalUrl
+
+  if (id) {
+    finalUrl = eventsURL + '/' + id // INDIRIZZO SPECIFICO
+  } else {
+    finalUrl = eventsURL // INDIRIZZO GENERICO
+    // la POST si fa sempre sull'indirizzo GENERICO
+    // la PUT si fa sempre sull'indirizzo SPECIFICO
+  }
+
+  fetch(finalUrl, {
+    method: method, // creo una nuova risorsa
+    // method: id ? 'PUT' : 'POST', // creo una nuova risorsa
     body: JSON.stringify(newEvent), // devo mandare newEvent in formato JSON!
     headers: {
       'Content-Type': 'application/json', // va messa! indica alle API che il contenuto è in formato JSON
--- scripts/details.js
+++ scripts/details.js
@@ -18,3 +18,66 @@ console.log(url)
 const allTheParameters = new URLSearchParams(url)
 const id = allTheParameters.get('concertID')
 console.log('ID', id)
+
+const eventsURL = 'https://striveschool-api.herokuapp.com/api/agenda'
+
+// b) recupero i dettagli
+const getDetails = function () {
+  fetch(eventsURL + '/' + id)
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw new Error(res.status)
+      }
+    })
+    .then((eventDetails) => {
+      console.log('DETTAGLI EVENTO', eventDetails)
+      // ora con questi dati vado a riempire gli "spazi vuoti" che ho lasciato nella card
+      document.getElementById('name').innerText = eventDetails.name
+      document.getElementById('description').innerText =
+        eventDetails.description
+      document.getElementById('price').innerText = eventDetails.price + '€'
+      document.getElementById('time').innerText = eventDetails.time
+    })
+    .catch((err) => {
+      console.log('ERRORE NEL RECUPERO DETTAGLI', err)
+      document.getElementById('name').innerText = err
+    })
+}
+
+getDetails()
+
+// ELIMINAZIONE EVENTO
+const deleteEvent = function () {
+  fetch(eventsURL + '/' + id, {
+    method: 'DELETE',
+  })
+    .then((res) => {
+      if (res.ok) {
+        // elemento ELIMINATO
+        alert('CONCERTO ELIMINATO!')
+        // riportiamo l'utente in homepage
+        location.assign('./index.html')
+      } else {
+        // problema nell'eliminazione
+        throw new Error(`problema nella response: ${res.status}`)
+      }
+    })
+    .catch((err) => {
+      console.log('ERRORE IN ELIMINAZIONE', err)
+    })
+}
+
+const editEvent = function () {
+  // questa funzione deve riportarmi in backoffice con il parametro del concerto
+  // da modificare
+  location.assign('./backoffice.html?concertID=' + id)
+}
+
+// RECAP DEGLI URL
+// GET su 'https://striveschool-api.herokuapp.com/api/agenda' --> TUTTI I CONCERTI
+// GET su 'https://striveschool-api.herokuapp.com/api/agenda/690c73f54c1df3001589c89c' --> UN CONCERTO
+// POST su 'https://striveschool-api.herokuapp.com/api/agenda'
+// DELETE su 'https://striveschool-api.herokuapp.com/api/agenda/690c73f54c1df3001589c89c' --> UN CONCERTO
+// PUT su 'https://striveschool-api.herokuapp.com/api/agenda/690c73f54c1df3001589c89c'
